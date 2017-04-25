0173b4fd3e3db0bbb6d67104c8437eb649596519
wuyuexin <wuyuexins@gmail.com>
2017-04-25 17:54:00 +0800

    add animations for route

--- frontend/src/main/webapp/src/app/app.module.ts
+++ frontend/src/main/webapp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import {AppComponent} from "./app.component";
 import {HomeComponent} from "./home/home.component";
@@ -9,13 +10,15 @@ import {AppRouteModule} from "./app.routing.module";
 import {PageNotFoundComponent} from "./404/not-found.component";
 import {SignModule} from "./sign/sign.module";
 
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     SignModule,
-    AppRouteModule
+    AppRouteModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
--- frontend/src/main/webapp/src/app/sign/sign-up.component.ts
+++ frontend/src/main/webapp/src/app/sign/sign-up.component.ts
@@ -1,9 +1,26 @@
-import {Component} from "@angular/core";
+import {Component, HostBinding, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {slideInDownAnimation} from "../animations";
 
 @Component({
   template: `
-    <h3>This is Sign up</h3>
-  `
+    <h3 style="width: 100%">This is Sign up</h3>
+    <button (click)="signIn()" class="btn btn-default">Sign In</button>
+  `,
+  animations: [slideInDownAnimation]
 })
-export class SignUpComponent {
+
+export class SignUpComponent{
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'absolute';
+
+  constructor(private route: Router) {
+  }
+
+  signIn() {
+    console.log('Click Sign In');
+    this.route.navigate(['/signin', {id: '01', name: 'roysmond'}]);
+  }
 }

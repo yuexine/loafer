32370f3442178899a398aa3c7d23d4cb396429b4
wuyuexin <wuyuexins@gmail.com>
2017-04-27 14:13:21 +0800

    add Login page

--- frontend/src/main/webapp/src/app/sign/sign-in.component.ts
+++ frontend/src/main/webapp/src/app/sign/sign-in.component.ts
@@ -1,10 +1,19 @@
 import {Component} from "@angular/core";
+import {User} from "./user";
 
 @Component({
-  template: `
-    <h2>This is Sign In Component</h2>
-  `
+  // template: `
+  //   <h2>This is Sign In Component</h2>
+  // `
+  templateUrl: './sign-in.html',
+  styleUrls: ['./sign-in.css']
 })
 
 export class SignInComponent {
+
+  model = new User();
+
+  onSubmit() {
+    console.log(JSON.stringify(this.model));
+  }
 }

da50a208858ed92e59fe0f632d5dcfbf4d836a9f
wuyuexin <wuyuexins@gmail.com>
2017-05-02 14:11:58 +0800

    Add user login domain

---
+++ backend/src/main/java/loafer/domain/PersistentToken.java
@@ -0,0 +1,70 @@
+package loafer.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * Created by w on 2017/5/2.
+ */
+@Setter
+@Getter
+@Entity
+@Table(name = "lf_persistent_token")
+public class PersistentToken implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final int MAX_USER_AGENT_LEN = 255;
+
+    @Id
+    private String series;
+
+    @JsonIgnore
+    @NotNull
+    @Column(name = "token_value", nullable = false)
+    private String tokenValue;
+
+    @Column(name = "token_date")
+    private LocalDate tokenDate;
+
+    @Size(min = 0, max = 39)
+    @Column(name = "ip_address", length = 39)
+    private String ipAddress;
+
+    @Column(name = "user_agent")
+    private String userAgent;
+
+    @JsonIgnore
+    @ManyToOne
+    private User user;
+
+    public void setUserAgent(String userAgent) {
+        if (userAgent.length() > MAX_USER_AGENT_LEN) {
+            this.userAgent = userAgent.substring(0, MAX_USER_AGENT_LEN - 1);
+        } else {
+            this.userAgent = userAgent;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersistentToken)) return false;
+
+        PersistentToken that = (PersistentToken) o;
+
+        return getSeries() != null ? getSeries().equals(that.getSeries()) : that.getSeries() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getSeries() != null ? getSeries().hashCode() : 0;
+    }
+}
---
+++ backend/src/main/java/loafer/domain/User.java
@@ -0,0 +1,80 @@
+package loafer.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by w on 2017/5/2.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "lf_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+//    @Pattern(regexp = "")
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String username;
+
+    @JsonIgnore
+    @NotNull
+    @Size(min = 60, max = 60)
+    private String password;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean activated = false;
+
+    @JsonIgnore
+    @Size(max = 256)
+    @Column(name = "activation_key", length = 20)
+    private String activationKey;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "lf_user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")}
+    )
+    private Set<Authority> authorities = new HashSet<>();
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "user")
+    private Set<PersistentToken> persistentTokens = new HashSet<>();
+
+    private void setUsername(String username) {
+        this.username = username.toLowerCase();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getUsername().equals(user.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return getUsername().hashCode();
+    }
+}

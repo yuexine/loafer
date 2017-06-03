bcf3009df7071f00295ddd06b640b2d9ce04c09e
wuyuexin <wuyuexins@gmail.com>
2017-06-03 19:05:54 +0800

    add some static html and css page

--- frontend/src/main/frontend/src/app/component/article/article.component.ts
+++ frontend/src/main/frontend/src/app/component/article/article.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-  templateUrl: './article.component.html'
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css', '../discovery/discovery.component.css']
 })
 
 export class ArticleComponent {
--- frontend/src/main/frontend/src/app/component/topic/topic.component.ts
+++ frontend/src/main/frontend/src/app/component/topic/topic.component.ts
@@ -1,7 +1,9 @@
 import {Component} from "@angular/core";
 
 @Component({
-  templateUrl: './topic.component.html'
+  templateUrl: './topic.component.html',
+  styleUrls: ['../discovery/discovery.component.css', './topic.component.css']
 })
 
-export class TopicComponent{}
+export class TopicComponent {
+}
